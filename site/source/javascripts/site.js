c24867705548bfdf32f97e139170e6ad3c463e8a
Unknown <levine.1726@osu.edu>
2018-08-17 13:44:03 -0400

    Updated contact form, added validation checking.
    Updated the HTML for the contact form for better text input support. Also added message validation by adding the jQuery Validation plugin to the project.

--- site/source/javascripts/site.js
+++ site/source/javascripts/site.js
@@ -1,7 +1,56 @@
 // This is where it all goes :)
 //= require jquery
+//= require jquery-validation/dist/jquery.validate.min.js
 //= require materialize-css/dist/js/materialize
 
-$(document).ready(function () {
-	M.AutoInit();
-});
+
+(function (){
+	$(document).ready(function () {
+		//Automatically initalize all Materialize JS components
+		M.AutoInit();
+
+		//Register event listener for the contact submit and cancel buttons
+		$('#submit').click(submit);
+
+		//setup contact form validation using the jQuery Validation plugin
+		 $("#email-form").validate({
+	        rules: {
+	            name: {
+	                required: true,
+	            },
+	            email: {
+	                required: true,
+	                email:true
+	            },
+				subject: {
+					required: true
+					
+				},
+				message: {
+	                required: true
+	            }
+		     },
+		     errorElement : 'div'
+		 });
+
+		//Bind event to modal to close open tooltips on a click
+		$('.modal').click(function () {
+			$(".tooltipped").each(function () {
+				var tooltipElt =  M.Tooltip.getInstance(this);
+				if (tooltipElt != undefined) {
+					tooltipElt.destroy();
+				}
+			});
+		});
+	});
+
+	
+	/**
+	* Event function to register to the submit button of our contact form.
+	*/
+	function submit(event) {
+		$('#email-form').submit();
+	}
+
+}());
+
